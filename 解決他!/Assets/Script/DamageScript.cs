1c4a8f8b903286df6fea17fb8d0ee0f297fd07db
amdr88611 <52878445+amdr88611@users.noreply.github.com>
2019-07-23 19:45:34 +0800

    再傳一次

---
+++ 解決他!/Assets/Script/DamageScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageScript : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        print("打到玩家了");
+    }
+}
